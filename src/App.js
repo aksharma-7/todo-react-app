443aaa9c5a89a1441fa4679d9f4234dd08f0feda
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-24 20:22:16 +0530

    Added Style and passed props down the components

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ class App extends React.Component {
     ]
   }
 
+  markComplete = (id) => {
+    console.log(id)
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
